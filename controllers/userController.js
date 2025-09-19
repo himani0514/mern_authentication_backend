670275b356616459a999c8c075d5c9d0152e9dcb
himani0514 <tembharehimani48@gmail.com>
2025-09-20 00:25:12 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -241,6 +241,8 @@ export const logout = catchAsyncError(async (req, res, next) => {
     .cookie("token", "", {
       expires: new Date(Date.now()),
       httpOnly: true,
+      secure:true,
+      sameSite:"None"
     })
     .json({
       success: true,
